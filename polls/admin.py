132881d10a08c543e9a894b3a255b6f763890032
Jason McKerr <mckerrj@gmail.com>
2014-02-10 09:48:20 -0500

    add entry

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from polls.models import Poll, Choice
+from polls.models import Poll, Choice, Entry
 
 
 admin.site.register(Poll)
 admin.site.register(Choice)
+admin.site.register(Entry)
