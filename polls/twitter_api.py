36f083f14b8aa519e2bb2dbae5f69f8b95833fd9
Jason McKerr <mckerrj@gmail.com>
2014-02-10 16:24:26 -0500

    remove.

--- polls/twitter_api.py
+++
@@ -1,8 +0,0 @@
-from polls.models import Tweet
-from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
-
-
-class TweetResource(ModelResource):
-    class Meta:
-        queryset = Tweet.objects.all()
-        resource_name = 'tweet'
