adc892bcd278f8afe26c222fcc05516bc6546aeb
Jason McKerr <mckerrj@gmail.com>
2014-02-12 18:47:36 -0500

    refactoring.  Still don't totally like the setup for creating data, and calling.  It ends up getting called to much.

--- polls/tests/test_twitter_sync.py
+++ polls/tests/test_twitter_sync.py
@@ -5,17 +5,23 @@ from polls.models import Tweet, TwitterUser
 
 # todo change to mock objects
 class TestsTweetResource(ResourceTestCase):
+
     def setUp(self):
         super(TestsTweetResource, self).setUp()
+        self.test_call_for_timeline_data()
+
+    def test_call_for_timeline_data(self):
+        self.data = twitter_sync.call_for_timeline_data_json()
+        self.assertValidJSON(self.data.content)
 
     def test_get_sync_twitter_user_timeline(self):
-        data = twitter_sync.call_for_timeline_data_json()
-        twitter_sync.sync_tweets_and_users(data)
+        twitter_sync.sync_tweets_and_users(self.data.json())
         self.assertEquals(TwitterUser.objects.all().count(), 1)
         self.assertEquals(Tweet.objects.all().count(), 5)
 
     def test_get_sync_twitter_user_timeline_simple(self):
-        data = twitter_sync.call_for_timeline_data_json()
-        twitter_sync.sync_tweets_and_users_simplesave(data)
+        twitter_sync.sync_tweets_and_users_simplesave(self.data.json())
         self.assertEquals(TwitterUser.objects.all().count(), 1)
         self.assertEquals(Tweet.objects.all().count(), 5)
+
+
