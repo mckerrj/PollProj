b2178e492730c16fd9978972ce38dddc42e44007
mckerrj <mckerrj@gmail.com>
2014-02-25 10:45:24 -0500

    add twitteruser and tweet to admin

--- twitter/admin.py
+++ twitter/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from twitter.models import Poll, Choice, Entry
+from twitter.models import Poll, Choice, Entry, TwitterUser, Tweet
 
 
 admin.site.register(Poll)
 admin.site.register(Choice)
 admin.site.register(Entry)
+admin.site.register(TwitterUser)
+admin.site.register(Tweet)
